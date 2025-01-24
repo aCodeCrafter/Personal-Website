a2e50caafd93040f57b553c85a2e9bfaa95bcf34
Liam Callahan <acodecrafter@gmail.com>
2025-01-24 07:13:32 -0500

    Added languages and blog preview section. Also moved js to seperate file

--- server.py
+++ server.py
@@ -3,5 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def test():
+def main_page():
   return render_template("main.html")
+
+@app.route("/blog")
+def blog_page():
+  return render_template("blog.html")
+
+# ADD blog-json endpoint
+# ADD /rss endpoint
